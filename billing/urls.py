77002defc53deeee1678e3b9a362bd02bebcdf49
Abhi1517621 <ee240002041@iiti.ac.in>
2026-06-20 13:14:44 +0530

    Add files via upload

---
+++ billing/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('patient/pay/<uuid:invoice_id>/', views.pay_invoice, name='pay_invoice'),
+    path('patient/invoice/<uuid:invoice_id>/', views.invoice_details, name='invoice_details'),
+]
